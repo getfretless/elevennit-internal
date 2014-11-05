e32ac29ff51a29c4d625b0901b9ea090780ed848
Dave Strus <dave@davestrus.com>
2014-11-05 16:43:08 -0500

    Set default scopes for Post and Category.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,6 @@
 class Category < ActiveRecord::Base
   has_many :posts
+  default_scope { order(:name) }
   validates :name, length: { maximum: 20 }, presence: true
   validates :title, length: { maximum: 100 }
   validates :description, length: { maximum: 500 }
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ActiveRecord::Base
   belongs_to :category
+  default_scope { order('updated_at DESC').includes(:category) }
   validates :title, length: { maximum: 255 }, presence: true
+  validates :category_id, presence: true
   enum post_type: [:link, :text]
 end
