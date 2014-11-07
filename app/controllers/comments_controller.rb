54592f8b50e2dd8dee8b9a318ed262d4c66436a4
Dave Strus <dave@davestrus.com>
2014-11-06 19:39:02 -0500

    Create threaded comments.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,12 +2,23 @@ class CommentsController < ApplicationController
   def create
     post = Post.find params[:comment][:commentable_id]
     comment = Comment.build_from post, current_user.id, comment_params[:body]
+    comment.parent_id = params[:comment][:parent_id]
     if comment.save
       flash[:notice] = 'Your comment has been added.'
     else
       flash[:error] = 'There was a problem adding your comment.'
     end
     redirect_to post
+  end
+
+  def new
+    @parent = Comment.find params[:parent_id]
+    post = @parent.commentable
+    @comment = Comment.build_from(post, current_user.id, '')
+    @comment.parent_id = @parent.id
+    respond_to do |format|
+      format.js {}
+    end
   end
 
   private
